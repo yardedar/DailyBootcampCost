fcb4d7b6092cd973a5b055cb360858576c76e856
Ryan Heinrich <heinrich.ryan@gmail.com>
2021-09-24 09:57:24 -0400

    Add project files.

---
+++ DailyBootcampCost/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DailyBootcampCost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var CostOfBootcamp = 15500.0;
+            var NumberOfDays = 65;
+            var PricePerDay = CostOfBootcamp / NumberOfDays;
+            Console.WriteLine($"The daily cost of boot camp is ${PricePerDay}");
+        }
+    }
+}
